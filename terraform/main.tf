281ae9752121b594a2db3189390ff1f5858f357e
chandrashekar <chandrun2989@gmail.com>
2025-12-19 20:10:49 +0530

    testing ec2 module

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,5 +3,5 @@ module "EC2" {
   source                = "./modules/EC2"
   CPU                   = var.CPU
   AMI                   = var.AMI
-  KEY_NAME              = module.KEY.KEY_NAME
+  KEY_NAME              = var.key_name
 }
