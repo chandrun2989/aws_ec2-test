bacf303eff7de8be2197ed8e24c90acc8725e906
chandrashekar <chandrun2989@gmail.com>
2025-12-19 20:25:39 +0530

    testing ec2 module

--- terraform/modules/EC2/outputs.tf
+++ terraform/modules/EC2/outputs.tf
@@ -1,14 +1,14 @@
 output "instance_id" {
   description = "EC2 Instance ID"
-  value       = aws_instance.servers.id 
+  value       = { for k, v in aws_instance.servers : k => v.id }
 }
 
 output "public_ip" {
   description = "EC2 Public IP"
-  value       = aws_instance.servers.public_ip
+  value       = { for k, v in aws_instance.servers : k => v.public_ip }
 }
 
 output "public_dns" {
   description = "EC2 Public DNS"
-  value       = aws_instance.servers.public_dns
+  value       = { for k, v in aws_instance.servers : k => v.public_dns }
 }
