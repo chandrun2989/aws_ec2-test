eb92b69676e94f7e23421686599f86e2df60dc17
chandrashekar <chandrun2989@gmail.com>
2025-12-19 20:00:47 +0530

    testing ec2 module

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,19 +1,6 @@
-data "aws_vpc" "default" {
-  default = true
-}
-
-# Grab subnets from the default VPC and pick one
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
-
-
 # EC2 instance
 module "EC2" {
-  source                = "../module/EC2"
+  source                = "modules/EC2"
   VPC_ID                = data.aws_vpc.default.id
   CPU                   = var.CPU
   AMI                   = var.AMI
--- terraform/modules/EC2/main.tf
+++ terraform/modules/EC2/main.tf
@@ -7,3 +7,14 @@ resource "aws_instance" "servers" {
     Name = each.value.name
   }
 }
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Grab subnets from the default VPC and pick one
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
